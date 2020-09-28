f982bcbc04840de6059996bfdaa689257bbaeff9
Othon Brück <othon.bruck@hotmail.com>
2020-09-28 15:07:33 -0300

    feat: Estilizando componente Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,24 +1,11 @@
 import React, { Component } from "react";
-import { Grid, Segment } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 
 export default class Formulario extends Component {
   render() {
     return (
-      <Grid columns={3} divided>
-        <Grid.Row stretched>
-          <Grid.Column>
-            <Segment></Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>1</Segment>
-            <Segment>2</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>1</Segment>
-            <Segment>2</Segment>
-            <Segment>3</Segment>
-          </Grid.Column>
-        </Grid.Row>
+      <Grid columns={3} divided className="formulario">
+        <Grid.Row stretched></Grid.Row>
       </Grid>
     );
   }
