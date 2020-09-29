75131a07b0ac13e5d7da36f9868bf69919970033
Othon Brück <othon.bruck@hotmail.com>
2020-09-29 15:17:42 -0300

    feat: Fazendo importações e alterando forms para redux-form]

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,50 +1,50 @@
 import React, { Component } from "react";
-import { Grid } from "semantic-ui-react";
+import { Field, reduxForm } from "redux-form";
+import { Grid, Button } from "semantic-ui-react";
 
 export default class Formulario extends Component {
   render() {
     return (
-      <Grid columns={2} className="formulario">
-        <Grid.Row stretched>
+      <Grid className="formulario">
+        <Grid.Row>
           <Grid.Column width={8} textAlign="right"></Grid.Column>
           <Grid.Column width={8}></Grid.Column>
         </Grid.Row>
-        <Grid.Row stretched>
+        <Grid.Row>
           <Grid.Column width={16} textAlign="center">
-            campo
-            <input></input>
+            <Field name="inputName" component="input" type="text" />
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row stretched>
+        <Grid.Row>
           <Grid.Column width={16} textAlign="center">
-            campo
-            <input></input>
+            <Field name="inputName" component="input" type="text" />
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row stretched>
+        <Grid.Row>
           <Grid.Column width={16} textAlign="center">
-            campo
-            <input></input>
+            <Field name="inputName" component="input" type="text" />
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row stretched>
+        <Grid.Row>
           <Grid.Column width={16} textAlign="center">
-            campo
-            <input></input>
+            <Field name="inputName" component="input" type="text" />
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row stretched>
+        <Grid.Row>
           <Grid.Column width={16} textAlign="center">
-            campo
-            <input></input>
+            <Field name="inputName" component="input" type="text" />
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row stretched>
-          <Grid.Column width={16} textAlign="center">
-            <button>ss</button>
+        <Grid.Row className="botao">
+          <Grid.Column width={16}>
+            <Button className="search-button">Enviar</Button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
     );
   }
 }
+
+Formulario = reduxForm({
+  form: "contact",
+})(Formulario);
