1b54b509e08ff6885a66e30ac17a1ce0596a3210
Othon Brück <othon.bruck@hotmail.com>
2020-09-29 11:13:07 -0300

    feat: Instalação do redux form e prototipagem

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,8 +4,46 @@ import { Grid } from "semantic-ui-react";
 export default class Formulario extends Component {
   render() {
     return (
-      <Grid columns={3} divided className="formulario">
-        <Grid.Row stretched></Grid.Row>
+      <Grid columns={2} className="formulario">
+        <Grid.Row stretched>
+          <Grid.Column width={8} textAlign="right"></Grid.Column>
+          <Grid.Column width={8}></Grid.Column>
+        </Grid.Row>
+        <Grid.Row stretched>
+          <Grid.Column width={16} textAlign="center">
+            campo
+            <input></input>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row stretched>
+          <Grid.Column width={16} textAlign="center">
+            campo
+            <input></input>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row stretched>
+          <Grid.Column width={16} textAlign="center">
+            campo
+            <input></input>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row stretched>
+          <Grid.Column width={16} textAlign="center">
+            campo
+            <input></input>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row stretched>
+          <Grid.Column width={16} textAlign="center">
+            campo
+            <input></input>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row stretched>
+          <Grid.Column width={16} textAlign="center">
+            <button>ss</button>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     );
   }
