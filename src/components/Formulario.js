cfc5662af2279e0c37e5803ef1244482c3b228a1
Othon Brück <othon.bruck@hotmail.com>
2020-09-30 13:53:07 -0300

    feat: Configuração da API e ajustes no formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,46 +1,73 @@
 import React, { Component } from "react";
-import { Field, reduxForm } from "redux-form";
+import { Form, Field, reduxForm } from "redux-form";
 import { Grid, Button } from "semantic-ui-react";
 
 export default class Formulario extends Component {
   render() {
     return (
-      <Grid className="formulario">
-        <Grid.Row>
-          <Grid.Column width={8} textAlign="right"></Grid.Column>
-          <Grid.Column width={8}></Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={16} textAlign="center">
-            <Field name="inputName" component="input" type="text" />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={16} textAlign="center">
-            <Field name="inputName" component="input" type="text" />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={16} textAlign="center">
-            <Field name="inputName" component="input" type="text" />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={16} textAlign="center">
-            <Field name="inputName" component="input" type="text" />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={16} textAlign="center">
-            <Field name="inputName" component="input" type="text" />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row className="botao">
-          <Grid.Column width={16}>
-            <Button className="search-button">Enviar</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <Form>
+        <Grid className="formulario">
+          <Grid.Row>
+            <Grid.Column width={8} textAlign="right">
+              <Field
+                name="TiposSeguro"
+                component="select"
+                label="Tipos de Seguro"
+              >
+                <option value="#ff0000">Reddsadas </option>
+                <option value="#00ff00">Green</option>
+                <option value="#0000ff">Blue</option>
+              </Field>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Field
+                name="TiposCapital"
+                component="select"
+                label="Tipos de Capital"
+              >
+                <option value="#ff0000">Reddasdassd</option>
+                <option value="#00ff00">Green</option>
+                <option value="#0000ff">Blue</option>
+              </Field>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16} textAlign="center">
+              <Field
+                name="CNPJ"
+                component="input"
+                type="text"
+                label="Tipos de Seguro"
+              />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16} textAlign="center">
+              <Field name="RazaoSocial" component="input" type="text" />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16} textAlign="center">
+              <Field name="Email" component="input" type="text" />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16} textAlign="center">
+              <Field name="NúmeroAgencia" component="input" type="text" />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16} textAlign="center">
+              <Field name="NomeAgencia" component="input" type="text" />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row className="botao">
+            <Grid.Column width={16}>
+              <Button className="search-button">Enviar</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Form>
     );
   }
 }
