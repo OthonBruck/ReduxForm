c31fc0658415b6792b932a5706b1a442d7689537
Othon Brück <othon.bruck@hotmail.com>
2020-10-01 10:17:09 -0300

    feat: Validação de E-mail e Renderização dos input

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -16,6 +16,28 @@ const agenciaNormalizer = (value) => {
   return value
     .replace(/\D/g, '') // substitui qualquer caracter que nao seja numero por nada
 };
+
+const validate = values => {
+  const errors = {}
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+  return errors
+}
+
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type}/>
+      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+)
+
+
 export default class Formulario extends Component {
   render() {
     return (
@@ -24,7 +46,8 @@ export default class Formulario extends Component {
           <Grid.Row>
             <Grid.Column width={8} textAlign="right">
               <Field
-                name="TiposSeguro"
+                name="tiposseguro"
+                type="select"
                 component="select"
                 label="Tipos de Seguro"
               >
@@ -35,6 +58,7 @@ export default class Formulario extends Component {
             </Grid.Column>
             <Grid.Column width={8}>
               <Field
+              type="select"
                 name="TiposCapital"
                 component="select"
                 label="Tipos de Capital"
@@ -50,31 +74,31 @@ export default class Formulario extends Component {
               <Field
               maxLength={18}
                 name="CNPJ"
-                component="input"
+                component={renderField}
                 type="text"
-                label="Tipos de Seguro"
+                label="CNPJ"
                 normalize={cnpjNormalizer}
               />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width={16} textAlign="center">
-              <Field name="RazaoSocial" component="input" type="text" />
+              <Field name="RazaoSocial" component={renderField} type="text" label="Razão Social"/>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width={16} textAlign="center">
-              <Field name="Email" component="input" type="text" />
+              <Field name="email" component={renderField} type="text"  label="E-mail"/>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width={16} textAlign="center">
-              <Field maxLength={4} name="NúmeroAgencia" component="input" type="text" normalize={agenciaNormalizer} />
+              <Field maxLength={4} name="NúmeroAgencia" component={renderField}type="text" normalize={agenciaNormalizer} label="Número da Agencia" />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width={16} textAlign="center">
-              <Field name="NomeAgencia" component="input" type="text" />
+              <Field name="NomeAgencia" component={renderField} type="text" label="Nome da Agencia" />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row className="botao">
@@ -90,4 +114,5 @@ export default class Formulario extends Component {
 
 Formulario = reduxForm({
   form: "contact",
+  validate,
 })(Formulario);
