0f16c36aded5ffd85c674f069e7b6fbe91741dac
Othon Brück <othon.bruck@hotmail.com>
2020-10-02 15:18:03 -0300

    feat: Renderizando o select

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Form, Field, reduxForm } from "redux-form";
+import { Field, reduxForm, submit, Form } from "redux-form";
 import { Grid, Button, Select, Input } from "semantic-ui-react";
 
 const cnpjNormalizer = (value) => {
@@ -9,94 +9,170 @@ const cnpjNormalizer = (value) => {
     .replace(/(\d{3})(\d)/, "$1.$2")
     .replace(/(\d{3})(\d)/, "$1/$2")
     .replace(/(\d{4})(\d)/, "$1-$2")
-    .replace(/(-\d{2})\d+?$/, '$1');
+    .replace(/(-\d{2})\d+?$/, "$1");
 };
 
 const agenciaNormalizer = (value) => {
-  return value.replace(/\D/g, "")
-  .replace(/(\d{4})\d+?$/, '$1');
+  return value.replace(/\D/g, "").replace(/(\d{4})\d+?$/, "$1");
 };
 
 const validate = (values) => {
   const errors = {};
   if (!values.email) {
-    errors.email = "Required";
+    errors.email = <div className="erro">⚠ Obrigatório</div>;
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
+    errors.email = <div className="erro">⚠ E-mail Invalido</div>;
+  }
+  if (!values.tiposSeguros) {
+    errors.tiposSeguros = <div className="erro">⚠ Obrigatório</div>;
+  }
+
+  if (!values.tiposCapital) {
+    errors.tiposCapital = <div className="erro">⚠ Obrigatório</div>;
+  }
+
+  if (!values.cnpj) {
+    errors.cnpj = <div className="erro">⚠ Obrigatório</div>;
+  } else if (values.cnpj.length !== 18) {
+    errors.cnpj = <div className="erro">⚠ CNPJ inválido</div>;
+  }
+
+  if (!values.razaoSocial) {
+    errors.razaoSocial = <div className="erro">⚠ Obrigatório</div>;
+  }
+
+  if (!values.numeroAgencia) {
+    errors.numeroAgencia = <div className="erro">⚠ Obrigatório</div>;
+  } else if (values.numeroAgencia.length !== 4) {
+    errors.numeroAgencia = <div className="erro">⚠ Número inválido</div>;
   }
   return errors;
 };
 
 const renderField = ({
-  input,
   label,
-  type,
-  meta: { touched, error, warning },
-}) => (
-  <div>
-    <label>{label}</label>
-    <div>
-      <Input {...input} placeholder={label} type={type} />
-      {touched &&
-        ((error && <span>{error}</span>) ||
-          (warning && <span>{warning}</span>))}
-    </div>
-  </div>
-);
-
-const renderSelectField = ({
   input,
-  label,
-  meta: { touched, error },
-  children,
+  meta: { touched, invalid, error },
   ...custom
 }) => (
-  <div>
+  <Input
+    {...input}
+    label={label}
+    placeholder={label}
+    error={touched && invalid}
+    {...custom}
+  />
+);
+
+const createRenderer = (render) => ({ input, meta, label, ...rest }) => (
+  <div
+    className={[
+      meta.error && meta.touched ? "error" : "",
+      meta.active ? "active" : "",
+    ].join(" ")}
+  >
     <label>{label}</label>
-    <div>
-      <Select
-        errorText={touched && error}
-        {...input}
-        onChange={(event, index, value) => input.onChange(value)}
-        options={colors}
-        value={colors.value}
-        {...custom}
-      />
-    </div>
+    {render(input, label, rest)}
+    {meta.error && meta.touched && <span>{meta.error}</span>}
   </div>
 );
-const colors = [
-  { key: 'yellow', value: 'ye', text: 'Yellow' },
-  { key: 'black', value: 'bl', text: 'Black' },
-  { key: 'green', value: 'gr', text: 'Green' },
-  { key: 'red', value: 're', text: 'Red' },
-]
 
-export default class Formulario extends Component {
+const RenderSelect = createRenderer((input, label, { children }) => (
+  <select {...input}>{children}</select>
+));
+
+const colors = [
+  'A Coruña',
+  'Álava',
+  'Albacete',
+  'Alicante',
+  'Almería',
+  'Asturias',
+  'Ávila',
+  'Badajoz',
+  'Balearic Islands',
+  'Barcelona',
+  'Burgos',
+  'Cáceres',
+  'Cádiz',
+  'Cantabria',
+  'Castellón',
+  'Ciudad Real',
+  'Córdoba',
+  'Cuenca',
+  'Guipúzcoa',
+  'Gerona',
+  'Granada',
+  'Guadalajara',
+  'Huelva',
+  'Huesca',
+  'Jaén',
+  'La Rioja',
+  'Las Palmas',
+  'León',
+  'Lérida',
+  'Lugo',
+  'Madrid',
+  'Málaga',
+  'Murcia',
+  'Navarra',
+  'Orense',
+  'Palencia',
+  'Pontevedra',
+  'Salamanca',
+  'Santa Cruz de Tenerife',
+  'Segovia',
+  'Sevilla',
+  'Soria',
+  'Tarragona',
+  'Teruel',
+  'Toledo',
+  'Valencia',
+  'Valladolid',
+  'Vizcaya',
+  'Zamora',
+  'Zaragoza'
+];
+
+class Formulario extends Component {
+  submit = (values) => {
+    console.log("AJEITA ESSA PORRA");
+    console.log(values);
+  };
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.props.handleSubmit(this.submit)}>
         <Grid className="formulario">
           <Grid.Row>
             <Grid.Column width={8} textAlign="right">
               <Field
-                name="tiposseguro"
+                name="tiposSeguro"
                 type="select"
-                component={renderSelectField}
+                component={RenderSelect}
                 label="Tipos de Seguro"
               >
+                                <option />
+                {colors.map((colors) => (
+                  <option key={colors} value={colors}>
+                    {colors}
+                  </option>
+                ))}
               </Field>
             </Grid.Column>
             <Grid.Column width={8}>
               <Field
                 type="select"
-                name="TiposCapital"
-                component={renderSelectField}
+                name="tiposCapital"
+                component={RenderSelect}
                 label="Tipos de Capital"
               >
-                <option value="#ff0000">Red</option>
-                <option value="#00ff00">Green</option>
-                <option value="#0000ff">Blue</option>
+                <option />
+                {colors.map((colors) => (
+                  <option key={colors} value={colors}>
+                    {colors}
+                  </option>
+                ))}
               </Field>
             </Grid.Column>
           </Grid.Row>
@@ -104,7 +180,7 @@ export default class Formulario extends Component {
             <Grid.Column width={16} textAlign="center">
               <Field
                 maxLength={18}
-                name="CNPJ"
+                name="cnpj"
                 component={renderField}
                 type="text"
                 label="CNPJ"
@@ -115,7 +191,7 @@ export default class Formulario extends Component {
           <Grid.Row>
             <Grid.Column width={16} textAlign="center">
               <Field
-                name="RazaoSocial"
+                name="razaoSocial"
                 component={renderField}
                 type="text"
                 label="Razão Social"
@@ -136,7 +212,7 @@ export default class Formulario extends Component {
             <Grid.Column width={16} textAlign="center">
               <Field
                 maxLength={4}
-                name="NúmeroAgencia"
+                name="numeroAgencia"
                 component={renderField}
                 type="text"
                 normalize={agenciaNormalizer}
@@ -147,7 +223,7 @@ export default class Formulario extends Component {
           <Grid.Row>
             <Grid.Column width={16} textAlign="center">
               <Field
-                name="NomeAgencia"
+                name="nomeAgencia"
                 component={renderField}
                 type="text"
                 label="Nome da Agencia"
@@ -156,7 +232,9 @@ export default class Formulario extends Component {
           </Grid.Row>
           <Grid.Row className="botao">
             <Grid.Column width={16}>
-              <Button className="search-button">Enviar</Button>
+              <Button type="submit" className="search-button">
+                Enviar
+              </Button>
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -165,7 +243,7 @@ export default class Formulario extends Component {
   }
 }
 
-Formulario = reduxForm({
+export default Formulario = reduxForm({
   form: "contact",
   validate,
 })(Formulario);
